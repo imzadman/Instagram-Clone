edbd324c05b1d0495d7e0b37a2dfcc02de74b004
Muhammad Zaid <zaidmansha4@gmail.com>
2024-07-16 21:39:02 +0500

    sd

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function AuthLayout({ children, authentication = true }) {
   const [loading, setLoading] = useState(true);
@@ -22,8 +21,6 @@ export function AuthLayout({ children, authentication = true }) {
       <span className="loading loading-infinity loading-lg"></span>
     </>
   ) : (
-    <>
-      {children} <SpeedInsights />
-    </>
+    <>{children}</>
   );
 }
